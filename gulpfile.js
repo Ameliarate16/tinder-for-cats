1c702dd98f1f9c6b9ae31bd26ad388d47a9f7100
Amelia Morton <an.morton16@gmail.com>
2019-01-16 16:37:00 -0500

    package.json nonsense

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,69 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    babel = require('babelify'),
+    reload = browserSync.reload,
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    concat = require('gulp-concat'),
+    imageMin = require('gulp-imagemin'),
+    cleanCSS = require('gulp-clean-css'),
+    cssnano = require('cssnano'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    uglify = require('gulp-uglify');
 
-gulp.task('styles', function() {
-  return gulp.src('./dev/styles/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('./public/styles'))
-    .pipe(reload({stream: true}));
+gulp.task('bs', function () {
+    browserSync.init({
+        server: '.'
+    });
 });
 
-gulp.task('watch', () => {
-  gulp.watch('./dev/styles/**/*.scss', gulp.series('styles'));
-  gulp.watch('./dev/scripts/main.js', gulp.series('scripts'));
-  gulp.watch('*.html', reload);
+gulp.task('styles', function () {
+    return gulp.src('src/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('public/'))
+        .pipe(reload({ stream: true }));
 });
 
-gulp.task('scripts', () => {
-  return gulp.src('./dev/scripts/main.js')
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(gulp.dest('./public/scripts'))
-    .pipe(reload({stream: true}));
+gulp.task('js', function () {
+    return browserify({
+        extensions: ['.js'],
+        entries: ['./src/scripts/app.js'],
+        sourceType: 'module',
+        debug: true
+    })
+        .transform('babelify', {
+            sourceMaps: true,
+            presets: ['env']
+        })
+        .bundle()
+        .pipe(source('scripts/app.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('public/'))
+        .pipe(reload({ stream: true }));
 });
 
-gulp.task('browser-sync', () => {
-  browserSync.init({
-    server: '.'  
-  })
+gulp.task('images', function () {
+    gulp.src('src/assets/**/*')
+        .pipe(imageMin())
+        .pipe(gulp.dest('public/images'));
 });
 
-gulp.task('default', gulp.series('browser-sync','styles', 'scripts', 'watch'));
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function () {
+    gulp.watch(['src/**/*.js'], gulp.series('js'));
+    gulp.watch('src/**/*.scss', gulp.series('styles'));
+    gulp.watch('index.html', reload);
+});
+
+gulp.task('default', gulp.series(gulp.parallel('watch', 'styles', 'bs', 'js', 'images'), function () { }));
--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//api key:    8820e5a33c8a3a3ce8dab58ef814de13
+//api secret: 05193f3fdce7e005c28c59bcdeb71e3d
+//pet.getRandom
+
+async function catSearch(method){
+  const cat = await $.ajax({
+    url: `http://api.petfinder.com/${method}`,
+    method: "GET",
+    data: {
+      key: "8820e5a33c8a3a3ce8dab58ef814de13",
+      format: "json",
+      callback: "?"
+    }
+  });
+}
+
 class App extends Component {
   render() {
     return (
